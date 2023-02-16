ecce8ba8c6a9cd6f88e750e6076aed5bd3f98fb2
myUsername <myemail@gmail.com>
2023-02-16 14:54:54 -0800

    5-printf.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,23 @@
  * Return: Always 0 (Success)
  */
 
+
 int main(void)
+
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long in: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+	int a;
+	char b;
+	float c;
+
+
+	printf("Size of a int: %lu byte(s)\n", (sizeof(a)));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)(sizeof(a)));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)(sizeof(a)));
+	char name[] = "ALX School";
+	puts("\"My name is Wesley");
+	puchar(name[0]);
+	puchar('\n);
 	return (0);
+
 }
