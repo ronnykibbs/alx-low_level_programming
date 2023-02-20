1578562a8c87efec3c160af73bc527df7e40f4ba
myUsername <myemail@gmail.com>
2023-02-20 10:15:05 -0800

    101-quote.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - A c program that print  the various types of computers
  *
  * Return: Always 0 (Success)
  */
-
-
 int main(void)
-
 {
-
-	int a;
-	char b;
-	float c;
-
-
-	printf("Size of a int: %lu byte(s)\n", (sizeof(a)));
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)(sizeof(a)));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)(sizeof(a)));
-	char name[] = "ALX School";
-	puts("\"My name is Wesley");
-	puchar(name[0]);
-	puchar('\n);
-	return (0);
-
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	 printf("Size of an int: %d byte(s)\n", sizeof(int));
+	  printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	   printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	    printf("Size of a float: %d byte(s)\n", sizeof(float));
+	    return (0);
 }
