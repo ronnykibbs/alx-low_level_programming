41d7afbffa7ec3b79e6211be211351f4ab68b5f5
Your <you@example.com>
2023-04-05 13:56:58 -0700

    Write a function that returns the nth node of a listint_t linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,7 @@
  * @head: pointer to the head of the linked list
  * @index: index of the node to return
  *
- * Return: pointer to the nth node of the linked list, NULL if the node doesn't exist
+ * Return:pointer to the node we're looking for, or NULL
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
